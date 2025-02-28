d0bb5507d6ba3c81264dc6277a46cfe3d54a1f6f
long9657 <longlehien9657@gmail.com>
2025-03-01 01:44:19 +0700

    new config

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,61 +1,61 @@
 local is_neovide = vim.g.neovide or false
 
 return {
-  {
-    "stevearc/conform.nvim",
-    -- event = 'BufWritePre', -- uncomment for format on save
-    opts = require "configs.conform",
-  },
+    {
+        "stevearc/conform.nvim",
+        -- event = 'BufWritePre', -- uncomment for format on save
+        opts = require "configs.conform",
+    },
 
-  -- These are some examples, uncomment them if you want to see them work!
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      require "configs.lspconfig"
-    end,
-  },
-  {
-    "lukas-reineke/indent-blankline.nvim",
-    main = "ibl",
-    ---@module "ibl"
-    ---@type ibl.config
-    config = function()
-      require "configs.indent_blankline"
-    end,
-},
- { 
-  "CRAG666/betterTerm.nvim",
-  event = "VeryLazy",
-  opts = {
-      position = "bot",
-      size = 15,
-    },    
-  config = function() 
-    require "configs.betterTerm"
-  end,
-},
-{
-  "sphamba/smear-cursor.nvim",
-  event = "VeryLazy",
-  enabled = not(is_neovide),
-  opts = require "configs.smear_cursor",
-},
-{ 
-  "CRAG666/code_runner.nvim",
-  event = "VeryLazy",
-  config = function() 
-     require "configs.code_runner"
-  end,
-}
+    -- These are some examples, uncomment them if you want to see them work!
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            require "configs.lspconfig"
+        end,
+    },
+    {
+        "lukas-reineke/indent-blankline.nvim",
+        main = "ibl",
+        ---@module "ibl"
+        ---@type ibl.config
+        config = function()
+            require "configs.indent_blankline"
+        end,
+    },
+    {
+        "CRAG666/betterTerm.nvim",
+        event = "VeryLazy",
+        opts = {
+            position = "bot",
+            size = 15,
+        },
+        config = function()
+            require "configs.betterTerm"
+        end,
+    },
+    {
+        "sphamba/smear-cursor.nvim",
+        event = "VeryLazy",
+        enabled = not (is_neovide),
+        opts = require "configs.smear_cursor",
+    },
+    {
+        "CRAG666/code_runner.nvim",
+        event = "VeryLazy",
+        config = function()
+            require "configs.code_runner"
+        end,
+    }
 
 
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
+    -- {
+    -- 	"nvim-treesitter/nvim-treesitter",
+    -- 	opts = {
+    -- 		ensure_installed = {
+    -- 			"vim", "lua", "vimdoc",
+    --      "html", "css"
+    -- 		},
+    -- 	},
+    -- },
 }
